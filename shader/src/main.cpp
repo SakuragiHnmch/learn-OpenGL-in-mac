5bc5fd94f04e648269c16bd648d5663b50ad4258
兰俊康 <junkang.lan@ivydad.com>
2021-01-15 13:56:02 +0800

    clear depth buffer

--- shader/src/main.cpp
+++ shader/src/main.cpp
@@ -49,6 +49,7 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    glEnable(GL_DEPTH_TEST);
 
     // build and compile our shader program
     // ------------------------------------
@@ -192,7 +193,7 @@ int main()
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
