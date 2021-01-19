db510295955c8b0c95a52144aa2e6f6fdd09c8b0
兰俊康 <junkang.lan@ivydad.com>
2021-01-19 09:50:24 +0800

    deal with the alpha channel of awesomeface

--- getting-started/src/main.cpp
+++ getting-started/src/main.cpp
@@ -110,9 +110,9 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    data = stbi_load("../image/luffy.jpeg", &width, &height, &channels, 0);
+    data = stbi_load("../image/awesomeface.png", &width, &height, &channels, 0);
     if(data){
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }else{
         std::cout << "failed to load texture2 resources" << std::endl;
