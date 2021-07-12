c9bc7be12a452953b04339729c8b163a05128f43
junkang <junkang.lan@ivydad.com>
2021-07-12 21:08:35 +0800

    update: rewrite shader

--- courses/1-getting-started/resource/shader/vertex.glsl
+++ courses/1-getting-started/resource/shader/vertex.glsl
@@ -1,14 +1,12 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
 
-out vec2 TexCoord;
+out vec3 ourColor;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform vec3 offset;
 
-void main () {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+void main() {
+    gl_Position = vec4(aPos + offset, 1.0);
+    ourColor = aPos;
 }
