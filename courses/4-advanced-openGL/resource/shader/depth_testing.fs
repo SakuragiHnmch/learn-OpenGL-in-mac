3f802963a28d3e7d3965d7799cd66015a4e15830
junkang <754089122@qq.com>
2022-03-02 17:03:40 +0800

    update: stencil test

--- courses/4-advanced-openGL/resource/shader/depth_testing.fs
+++ courses/4-advanced-openGL/resource/shader/depth_testing.fs
@@ -1,14 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;
-in vec3 Position;
+in vec2 TexCoords;
 
-uniform vec3 cameraPos;
-uniform samplerCube skybox;
+uniform sampler2D texture1;
 
 void main() {
-    vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
-    FragColor = vec4(texture(skybox, R).rgb, 1.0);
+    FragColor = texture(texture1, TexCoords);
 }
